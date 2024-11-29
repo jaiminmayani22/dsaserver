29d03c995992a16afcada208586bad749f298732
Jaimin Patel <jaimin.pixerfect@gmail.com>
2024-11-29 11:48:04 +0530

    webhook udated

--- controller/webhook.controller.js
+++ controller/webhook.controller.js
@@ -100,11 +100,12 @@ async function updateWhatsappStatuses(data) {
                 const value = change.value;
                 if (value.statuses) {
                     for (const status of value.statuses) {
+                        const reason = status.errors?.[0]?.error_data?.details || status.errors?.[0]?.message || "Unknown error";
                         await MESSAGE_LOG.findOneAndUpdate(
                             { waMessageId: status.id },
                             {
                                 status: status.status,
-                                reason: status.errors[0]?.error_data.details,
+                                reason: reason,
                                 updatedAt: new Date()
                             },
                             { returnDocument: "after" }
